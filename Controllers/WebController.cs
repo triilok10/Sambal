e4339685ac877e551dda01adfdab784560358f97
TRILOK CHAND SWAMI <ittiku3@gmail.com>
2025-01-12 00:28:30 +0530

    Working to Create the API

--- Controllers/WebController.cs
+++ Controllers/WebController.cs
@@ -1,10 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Sambal.AppCode;
 using Sambal.Models;
+using System.Text;
 
 namespace Sambal.Controllers
 {
     public class WebController : Controller
     {
+
+        #region "Constructor"
+
+        private readonly HttpClient _httpClient;
+        private readonly dynamic baseUrl;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IClsSession _clsSession;
+        public WebController(HttpClient httpClient, IHttpContextAccessor httpContextAccessor, IClsSession clsSession)
+        {
+            _httpClient = httpClient;
+            _clsSession = clsSession;
+            _httpContextAccessor = httpContextAccessor;
+            var request = _httpContextAccessor.HttpContext.Request;
+            baseUrl = $"{request.Scheme}://{request.Host.Value}/";
+            _httpClient.BaseAddress = new Uri(baseUrl);
+        }
+
+        #endregion
+
+
         public IActionResult Index()
         {
             return View();
@@ -15,6 +38,39 @@ namespace Sambal.Controllers
             List<Web> lstWeb = new List<Web>();
             ViewBag.lstWeb = lstWeb;
             return View();
+        }
+
+        [HttpPost]
+        public async IActionResult AddCategory(Web pWeb, IFormFile CategoryImage)
+        {
+            Web obj = new Web();
+            if (pWeb == null)
+            {
+                pWeb.Message = "Please pass the required Data";
+                pWeb.Status = false;
+                return RedirectToAction("", "");
+            }
+            if (string.IsNullOrWhiteSpace(pWeb.CategoryTitle))
+            {
+                pWeb.Message = "Please pass the Category Title.";
+                pWeb.Status = false;
+                return RedirectToAction("", "");
+            }
+            if (CategoryImage.Length == 0)
+            {
+                pWeb.Message = "Please pass the Category Image";
+                pWeb.Status = false;
+                return RedirectToAction("", "");
+            }
+
+            //API URL 
+            string apiURL = baseUrl + "";
+
+            string Json = JsonConvert.SerializeObject(CategoryImage);
+            StringContent con = new StringContent(Json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _httpClient.PostAsync(apiURL, con);
+
+            return RedirectToAction("", ""); ;
         }
 
     }
--- Models/Web.cs
+++ Models/Web.cs
@@ -11,5 +11,9 @@
         public string CategoryDescription { get; set; }
         //CategoryImage
         public IFormFile CategoryImage { get; set; }
+        //Status
+        public bool Status { get; set; }
+        //Message
+        public string Message { get; set; }
     }
 }
